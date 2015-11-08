a2405f4761207c969c5fbbe642b363259c260682
David Shanske <david@shanske.com>
2015-11-08 11:18:51 -0600

    Cleanup

--- includes/class-sem-push.php
+++ includes/class-sem-push.php
@@ -8,17 +8,17 @@ class sem_push {
 		// If Pushover for WordPress is installed, take over comment notifications
 		if ( function_exists( 'ckpn_new_comment' ) ) {
 			remove_action( 'comment_post', 'ckpn_new_comment', 10, 2 );
-			add_action( 'comment_post', array('sem_push', 'ckpn_new_comment'), 11, 2);
+			add_action( 'comment_post', array( 'sem_push', 'ckpn_new_comment' ), 11, 2 );
 			// For debugging purposes you can enable notifications on edit
 			// add_action( 'edit_comment', array('sem_push', 'ckpn_new_comment'), 11, 2);
 		}
 		// If Pushbullet for WordPress is installed, take over comment notification
-    if ( function_exists( 'fnpn_new_comment' ) ) {
-      remove_action( 'comment_post', 'fnpn_new_comment', 10, 2 );
-      add_action( 'comment_post', array('sem_push', 'fnpn_new_comment'), 11, 2);
+		if ( function_exists( 'fnpn_new_comment' ) ) {
+			remove_action( 'comment_post', 'fnpn_new_comment', 10, 2 );
+			add_action( 'comment_post', array( 'sem_push', 'fnpn_new_comment' ), 11, 2 );
 			// For debugging purposes you can enable notifications on edit
-      // add_action( 'edit_comment', array('sem_push', 'fnpn_new_comment'), 11, 2);
-    }
+			// add_action( 'edit_comment', array('sem_push', 'fnpn_new_comment'), 11, 2);
+		}
 
 	}
 
@@ -47,98 +47,97 @@ class sem_push {
 	}
 
 
-/**
- * Send Notifications for new comments
- * @param  int $comment_id The ID of the newly submitted comment
- * @return void
- */
-public static function ckpn_new_comment( $comment_id ) {
+	/**
+	 * Send Notifications for new comments
+	 * @param  int $comment_id The ID of the newly submitted comment
+	 * @return void
+	 */
+	public static function ckpn_new_comment( $comment_id ) {
 		$options      = ckpn_get_options();
 		$args = array();
-	$args = self::generate_args($comment_id);
-	if (!$args) {
-		return;
-	}
-	if ( $options['multiple_keys'] ) {
- 	   $args['token'] = ckpn_get_application_key_by_setting( 'new_comment' );
-	}
-	ckpn_send_notification( $args );
+		$args = self::generate_args( $comment_id );
+		if ( ! $args ) {
+			return;
+		}
+		if ( $options['multiple_keys'] ) {
+			$args['token'] = ckpn_get_application_key_by_setting( 'new_comment' );
+		}
+		ckpn_send_notification( $args );
 
-	// Check if we should notify the author as well
-	if ( $options['notify_authors'] ) {
-		$author_user_key = get_user_meta( $post_data->post_author, 'ckpn_user_key', true );
-		if ( $author_user_key != '' && $author_user_key != $options['api_key'] ) { // Only send if the user has a key and it's not the same as the admin key
-			// Notify the Author their post has a comment
-			$args['user'] = $author_user_key;
-			if ( $options['multiple_keys'] ) {
-				$args['token'] = ckpn_get_application_key_by_setting( 'notify_authors' );
+		// Check if we should notify the author as well
+		if ( $options['notify_authors'] ) {
+			$author_user_key = get_user_meta( $post_data->post_author, 'ckpn_user_key', true );
+			if ( $author_user_key != '' && $author_user_key != $options['api_key'] ) { // Only send if the user has a key and it's not the same as the admin key
+				// Notify the Author their post has a comment
+				$args['user'] = $author_user_key;
+				if ( $options['multiple_keys'] ) {
+					$args['token'] = ckpn_get_application_key_by_setting( 'notify_authors' );
+				}
+				ckpn_send_notification( $args );
 			}
-			ckpn_send_notification( $args );
 		}
 	}
-}
 
-/**
- * Send Notifications for new comments
- * @param  int $comment_id The ID of the newly submitted comment
- * @return void
- */
-public static function fnpn_new_comment( $comment_id ) {
-    $options      = fnpn_get_options();
-    $args = array();
-  $args = self::generate_args($comment_id);
-  if (!$args) {
-    return;
-  }
-  if ( $options['multiple_keys'] ) {
-     $args['token'] = fnpn_get_application_key_by_setting( 'new_comment' );
-  }
-  fnpn_send_notification( $args );
+	/**
+	 * Send Notifications for new comments
+	 * @param  int $comment_id The ID of the newly submitted comment
+	 * @return void
+	 */
+	public static function fnpn_new_comment( $comment_id ) {
+		$options      = fnpn_get_options();
+		$args = array();
+		$args = self::generate_args( $comment_id );
+		if ( ! $args ) {
+			return;
+		}
+		if ( $options['multiple_keys'] ) {
+			$args['token'] = fnpn_get_application_key_by_setting( 'new_comment' );
+		}
+		fnpn_send_notification( $args );
 
-  // Check if we should notify the author as well
-  if ( $options['notify_authors'] ) {
-    $author_user_key = get_user_meta( $post_data->post_author, 'fnpn_user_key', true );
-    if ( $author_user_key != '' && $author_user_key != $options['api_key'] ) { // Only send if the user has a key and it's not the same as the admin key
-      // Notify the Author their post has a comment
-      $args['user'] = $author_user_key;
-      if ( $options['multiple_keys'] ) {
-        $args['token'] = fnpn_get_application_key_by_setting( 'notify_authors' );
-      }
-      fnpn_send_notification( $args );
-    }
-  }
-}
+		// Check if we should notify the author as well
+		if ( $options['notify_authors'] ) {
+			$author_user_key = get_user_meta( $post_data->post_author, 'fnpn_user_key', true );
+			if ( $author_user_key != '' && $author_user_key != $options['api_key'] ) { // Only send if the user has a key and it's not the same as the admin key
+				// Notify the Author their post has a comment
+				$args['user'] = $author_user_key;
+				if ( $options['multiple_keys'] ) {
+					$args['token'] = fnpn_get_application_key_by_setting( 'notify_authors' );
+				}
+				fnpn_send_notification( $args );
+			}
+		}
+	}
 
 
 
 	public static function generate_args($comment_id) {
 		$args = array();
-	  $comment_data = get_comment( $comment_id );
-  	// This is not the comment we're looking for. Move Along.
-  	if ( $comment_data->comment_approved != '1' ) {
-   	 return false;
-  	}	
-		$post_title = get_the_title( $comment_data->comment_post_ID );
-		$type = get_comment_meta( $comment_id, 'semantic_linkbacks_type', true);
-		if (! $type) {
-			$type = get_comment_type($comment_id);
-			if ( 'comment' === $type ) {
-    		$type = 'reply';
-  		}
+		$comment_data = get_comment( $comment_id );
+		// This is not the comment we're looking for. Move Along.
+		if ( $comment_data->comment_approved != '1' ) {
+			return false;
 		}
-  	$strings = self::get_notification_titles();
+		$post_title = get_the_title( $comment_data->comment_post_ID );
+		$type = get_comment_meta( $comment_id, 'semantic_linkbacks_type', true );
+		if ( ! $type ) {
+			$type = get_comment_type( $comment_id );
+			if ( 'comment' === $type ) {
+				$type = 'reply';
+			}
+		}
+		$strings = self::get_notification_titles();
 		if ( 'reply' === $type ) {
 			$args['message'] = wp_strip_all_tags( $comment_data->comment_content );
-    	$args['url'] = get_comment_link( $comment_data );
-   		$args['url_title'] = 'View';
+			$args['url'] = get_comment_link( $comment_data );
+			$args['url_title'] = 'View';
+		} else {
+			$args['message'] = wp_strip_all_tags( SemanticLinkbacksPlugin::comment_text_excerpt( '', $comment_data ) );
 		}
-		else {
-			$args['message'] = wp_strip_all_tags( SemanticLinkbacksPlugin::comment_text_excerpt( '', $comment_data) );
-		}
-		$args['title'] = sprintf(	$strings[$type], get_comment_author($comment_id), $post_title);
+		$args['title'] = sprintf( $strings[$type], get_comment_author( $comment_id ), $post_title );
 
-		$args['title'] = apply_filters( 'ckpn_newcomment_subject', $args['title'], $comment_id );   
-  	$args['message']   = apply_filters( 'ckpn_newcomment_message', $args['message'], $comment_id );
+		$args['title'] = apply_filters( 'ckpn_newcomment_subject', $args['title'], $comment_id );
+		$args['message']   = apply_filters( 'ckpn_newcomment_message', $args['message'], $comment_id );
 		return $args;
 	}
 
